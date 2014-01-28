6ca2dc1460b67fff9c26e4ab336f87e0bd717ae7
shupelneker <aquezada1987@gmail.com>
2014-01-28 16:49:35 -0300

    commit inicial

---
+++ config/localhost.py
@@ -0,0 +1,8 @@
+config = {
+
+# This config file will be detected in localhost environment and values defined here will overwrite those in config.py
+'environment': "localhost",
+
+# ----> ADD MORE CONFIGURATION OPTIONS HERE <----
+
+}
